63cf9cb75f7b3b61e72f6165ca74840ad4e95d26
NitaLewska <Nita@ya.ru>
2022-06-02 06:37:37 +0300

    logos (png to svg; add logos on btns)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
 import "./Footer.css"
-import twitter_logo from "../img/Twitter.png"
-import facebook_logo from "../img/Facebook.png"
-import instagram_logo from "../img/Instagram.png"
-import github_logo from "../img/GitHub.png"
+import twitter_logo from "../img/Twitter.svg"
+import facebook_logo from "../img/Facebook.svg"
+import instagram_logo from "../img/Instagram.svg"
+import github_logo from "../img/GitHub.svg"
 
 export default function Footer() {
     return (
       <footer>
           <a href="https://twitter.com/nita_lewska"><img src={twitter_logo} alt="twitter_logo"></img></a>
-          <a href=""><img src={facebook_logo} alt="facebook_logo"></img></a>
-          <a href=""><img src={instagram_logo} alt="instagram_logo"></img></a>
+          <img src={facebook_logo} alt="facebook_logo"></img>
+          <img src={instagram_logo} alt="instagram_logo"></img>
           <a href="https://github.com/NitaLewska"><img src={github_logo} alt="github_logo"></img></a>
       </footer>
     );
